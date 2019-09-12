1f40525a82aabb5eb6ffc447131f559de8e88b6d
quzhichao <quzhichao@lunaon.com>
2019-09-12 10:18:24 +0800

    deprocess done

--- tfjs-layers/src/layers/deprocess.ts
+++ tfjs-layers/src/layers/deprocess.ts
@@ -15,14 +15,13 @@
 // Porting Note: In Python Keras, the padding layers are in convolutional.py,
 //   but we decided to put them in a separate file (padding.ts) for clarity.
 
-// import * as tfc from '@tensorflow/tfjs-core';
-import { serialization, Tensor } from '@tensorflow/tfjs-core';
+import * as tfc from '@tensorflow/tfjs-core';
+import { serialization, Tensor, tidy } from '@tensorflow/tfjs-core';
 
 import { Shape } from '../keras_format/common';
 import { Layer, LayerArgs } from '../engine/topology';
 
-import { Kwargs } from '../types';
-import { getExactlyOneShape } from '../utils/types_utils';
+import { getExactlyOneShape, getExactlyOneTensor } from '../utils/types_utils';
 
 export declare interface DeprocessStylizedImageLayerArgs extends LayerArgs {
   activation?: string;
@@ -37,7 +36,7 @@ export class DeprocessStylizedImage extends Layer {
       args = {};
     }
     super(args);
-    this.activation = args.activation == null ? "sigmoid" : args.activation;
+    this.activation = args.activation
   }
 
   computeOutputShape(inputShape: Shape | Shape[]): Shape | Shape[] {
@@ -45,8 +44,16 @@ export class DeprocessStylizedImage extends Layer {
     return inputShape;
   }
 
-  call(inputs: Tensor | Tensor[], kwargs: Kwargs): Tensor | Tensor[] {
-    return inputs;
+  call(inputs: Tensor | Tensor[]): Tensor | Tensor[] {
+    let x = getExactlyOneTensor(inputs)
+    return tidy(() => {
+      if (this.activation === 'tanh') {
+        x = tfc.mul(tfc.add(x, 1.0), 127.5)
+      } else {
+        x = tfc.mul(x, 255)
+      }
+      return x
+    })
   }
 
   getConfig(): serialization.ConfigDict {
